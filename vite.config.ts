80194ddbc7b5f58cfec69e7b5e2076ef2b1e35fe
Adam <adam@mudd.me.uk>
2026-08-08 13:00:33 +0100

    fix(build): type Vitest configuration correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   root: '.',
